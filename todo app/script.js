3047fcc05d819c6e93d8d3f710af157292f68458
prince rathod <Princerathod292080@gmail.com>
2025-12-01 22:30:57 +0530

    Refactor addTodo and removeTodo functions

--- todo app/script.js
+++ todo app/script.js
@@ -1,25 +1,26 @@
 function addTodo() {
-  const input = document.getElementById("todo-input"); // input box lo
-  const value = input.value.trim(); // jo likha hai usse le lo
+  const input = document.getElementById("todo-input");
+  const value = input.value.trim();
 
   if (value === "") {
     alert("Please enter a task!");
-    return; // agar khali hai to aage mat badho
+    return;
   }
 
-  const list = document.getElementById("todo-list"); // ul list pakdo
-  const listItem = document.createElement("li"); // li item banao
+  const list = document.getElementById("todo-list");
+  const listItem = document.createElement("li");
 
   listItem.innerHTML = `
     ${value}
     <button onclick="removeTodo(this)">Delete</button>
   `;
 
-  list.appendChild(listItem); // ul ke andar li add karo
-  input.value = ""; // input box reset karo
+  list.appendChild(listItem);
+  input.value = "";
 }
 
 function removeTodo(btn) {
-  const item = btn.parentElement; // jis li pe click hua use pakdo
-  item.remove(); // us li ko hata do
+  const item = btn.parentElement;
+  item.remove();
 }
+
